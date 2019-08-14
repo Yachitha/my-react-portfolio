c439b969667bd2faabf696bd7bc44560ecbc9ecd
Yachitha Sandaruwan <yachithasandaruwan@gmail.com>
2019-08-14 13:43:59 +0530

    Updates

---
+++ precache-manifest.b414c39d0b16c0d9df42291b3df2638e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f946f4b2b7d50ce7ef091bd4658145",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "56806d51a94d7f34a75c",
+    "url": "/my-react-portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3fc843a2800a7218d123",
+    "url": "/my-react-portfolio/static/js/2.0c108307.chunk.js"
+  },
+  {
+    "revision": "56806d51a94d7f34a75c",
+    "url": "/my-react-portfolio/static/js/main.c8a72ae7.chunk.js"
+  },
+  {
+    "revision": "261f008b374f2ce043a2",
+    "url": "/my-react-portfolio/static/js/runtime~main.4583e94b.js"
+  }
+]);
